b3a623601c67beefa3b1700977423ae2697e1afe
DanCrk <dancrkyt@gmail.com>
2022-01-03 01:22:47 -0600

    Slider funcional x3

--- app/src/main/java/com/ryutec/pokedexv1/iu/view/DetailsActivity.kt
+++ app/src/main/java/com/ryutec/pokedexv1/iu/view/DetailsActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
 import com.ryutec.pokedexv1.data.adapter.viewpager.SliderAdapter
 import com.ryutec.pokedexv1.data.model.pokemon.PokemonResponse
 import com.ryutec.pokedexv1.databinding.ActivityDetailsBinding
 import com.ryutec.pokedexv1.iu.viewmodel.PokemonViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.abs
 
 @AndroidEntryPoint
 class DetailsActivity : AppCompatActivity() {
@@ -50,9 +54,22 @@ class DetailsActivity : AppCompatActivity() {
 
     }
 
-    private fun setUpViewPager(){
+    private fun setUpViewPager() {
         adapter = SliderAdapter(sliderItems, binding.sliderSprites)
         binding.sliderSprites.adapter = adapter
+
+        binding.sliderSprites.clipToPadding = false
+        binding.sliderSprites.clipChildren = false
+        binding.sliderSprites.offscreenPageLimit = 3
+        binding.sliderSprites.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+        val compositePagerTransformer = CompositePageTransformer()
+        compositePagerTransformer.addTransformer(MarginPageTransformer(30))
+        compositePagerTransformer.addTransformer{page, position ->
+            val r = 1 - abs(position)
+            page.scaleY = 0.85f + r*0.25f
+        }
+        binding.sliderSprites.setPageTransformer(compositePagerTransformer)
     }
 
     private fun refrescar(){ if(!sliderItems.isNullOrEmpty()){ adapter.notifyDataSetChanged() }else{ showError() } }
